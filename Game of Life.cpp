8b98a0fd8f744b5d6d0ebd0da8a3905f1d60110d
Laniakea <114653972+EmanonLaniakea@users.noreply.github.com>
2022-12-14 21:09:51 +0800

    增加预设

--- Game of Life.cpp
+++ Game of Life.cpp
@@ -5,12 +5,12 @@
 #include"resource.h"
 
 #define CELL 10	//单个细胞大小，像素
-#define SIZE 60 //画布大小,每行列细胞数
+#define SIZE 70 //画布大小,每行列细胞数
 
 bool live[SIZE][SIZE] = { 0 };	//每个cell的存活状态
 bool temp[SIZE][SIZE] = { 0 };
 bool running = true;
-bool pulse = false;
+bool pulse = true;
 int rx[3] = { -1,0,1 };	//用于遍历周围九宫格
 int ry[3] = { -1,0,1 };
 int sleeptime = 10;
@@ -94,6 +94,12 @@ void start()
 	while (1) {
 		flushmessage();
 		getmessage(&m, -1);
+		if (m.message == WM_KEYDOWN) {
+			if (m.vkcode == VK_ESCAPE) {
+				running = false;
+				return;
+			}
+		}
 		if (m.message == WM_LBUTTONUP) {
 			if (m.x > 1078 && m.x < 1327) {
 				if (m.y > 415 && m.y < 499) {	//playground
@@ -102,9 +108,10 @@ void start()
 				}
 				if (m.y > 544 && m.y < 627) {	//presets
 					preset();
+					map();
+					return;
 				}
 				if (m.y > 671 && m.y < 754) {	//options
-
 				}
 			}
 		}
@@ -145,7 +152,7 @@ void control()
 			sleeptime += 10;
 		}
 		if (m.vkcode == VK_DOWN) {
-			sleeptime -= 10;
+			sleeptime -= 50;
 			if (sleeptime <= 0) {
 				sleeptime = 10;
 			}
@@ -192,6 +199,58 @@ void preset()
 {
 	closegraph();
 	initgraph(1400, 800, 0);
-	loadimage(NULL,L"PNG",MAKEINTRESOURCE(IDB_PNG1));
-	
+	loadimage(NULL, L"PNG", MAKEINTRESOURCE(IDB_PNG1));
+	ExMessage m;
+	while (1) {
+		flushmessage();
+		getmessage(&m, -1);
+		if (m.message == WM_KEYDOWN) {
+			if (m.vkcode == VK_ESCAPE) {
+				return;
+			}
+		}
+		if (m.message == WM_LBUTTONUP) {
+			if (m.x > 600 && m.x < 1400 && m.y>0 && m.y < 270) {	//gosper gliter gun
+				{
+					live[2+2][5] = true;
+					live[2+2][6] = true;
+					live[3+2][5] = true;
+					live[3+2][6] = true;
+					live[11+3][5] = true;
+					live[11+3][6] = true;
+					live[11+3][7] = true;
+					live[12+3][4] = true;
+					live[12+3][8] = true;
+					live[13+3][3] = true;
+					live[13+3][9] = true;
+					live[14+3][3] = true;
+					live[14+3][9] = true;
+					live[15+3][6] = true;
+					live[16+3][4] = true;
+					live[16+3][8] = true;
+					live[17+3][5] = true;
+					live[17+3][6] = true;
+					live[17+3][7] = true;
+					live[18+3][6] = true;
+					live[21+3][3] = true;
+					live[21+3][4] = true;
+					live[21+3][5] = true;
+					live[22+3][3] = true;
+					live[22+3][4] = true;
+					live[22+3][5] = true;
+					live[23+3][2] = true;
+					live[23+3][6] = true;
+					live[25+3][1] = true;
+					live[25+3][2] = true;
+					live[25+3][6] = true;
+					live[25+3][7] = true;
+					live[35+3][3] = true;
+					live[35+3][4] = true;
+					live[36+3][3] = true;
+					live[36+3][4] = true;
+				}
+				return;
+			}
+		}
+	}
 }
