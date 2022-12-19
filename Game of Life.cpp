4f7db6c7b0107c988aa702a65171fff9e43edad0
Laniakea <114653972+EmanonLaniakea@users.noreply.github.com>
2022-12-19 18:41:38 +0800

    完善预设

--- Game of Life.cpp
+++ Game of Life.cpp
@@ -236,6 +236,7 @@ void preset()
 		if (m.message == WM_LBUTTONUP) {
 			if (m.x > 600 && m.x < 1400 && m.y>0 && m.y < 270) {	//gosper gliter gun
 				pass = false;
+				pulse = true;
 				for (int i = 0; i < SIZE; i++) {
 					for (int j = 0; j < SIZE; j++) {
 						live[i][j] = 0;
@@ -278,6 +279,186 @@ void preset()
 					live[35 + 3][4] = true;
 					live[36 + 3][3] = true;
 					live[36 + 3][4] = true;
+				}
+				return;
+			}
+			if (m.x > 0 && m.x < 300 && m.y>400 && m.y < 600) {	// pulsar
+				pass = false;
+				pulse = true;
+				for (int i = 0; i < SIZE; i++) {
+					for (int j = 0; j < SIZE; j++) {
+						live[i][j] = 0;
+					}
+				}
+				{
+					live[2][4] = true;
+					live[2][5] = true;
+					live[2][6] = true;
+					live[2][10] = true;
+					live[2][11] = true;
+					live[2][12] = true;
+					live[7][4] = true;
+					live[7][5] = true;
+					live[7][6] = true;
+					live[7][10] = true;
+					live[7][11] = true;
+					live[7][12] = true;
+					live[9][4] = true;
+					live[9][5] = true;
+					live[9][6] = true;
+					live[9][10] = true;
+					live[9][11] = true;
+					live[9][12] = true;
+					live[14][4] = true;
+					live[14][5] = true;
+					live[14][6] = true;
+					live[14][10] = true;
+					live[14][11] = true;
+					live[14][12] = true;
+					live[4][2] = true;
+					live[4][7] = true;
+					live[4][9] = true;
+					live[4][14] = true;
+					live[5][2] = true;
+					live[5][7] = true;
+					live[5][9] = true;
+					live[5][14] = true;
+					live[6][2] = true;
+					live[6][7] = true;
+					live[6][9] = true;
+					live[6][14] = true;
+					live[10][2] = true;
+					live[10][7] = true;
+					live[10][9] = true;
+					live[10][14] = true;
+					live[11][2] = true;
+					live[11][7] = true;
+					live[11][9] = true;
+					live[11][14] = true;
+					live[12][2] = true;
+					live[12][7] = true;
+					live[12][9] = true;
+					live[12][14] = true;
+				}
+				return;
+			}
+			if (m.x > 0 && m.x < 300 && m.y>600 && m.y < 800) {	//penta
+				pass = false;
+				pulse = true;
+				for (int i = 0; i < SIZE; i++) {
+					for (int j = 0; j < SIZE; j++) {
+						live[i][j] = 0;
+					}
+				}
+				{
+					live[2 + 3][3 + 5] = true;
+					live[2 + 3][3 + 10] = true;
+					live[3 + 3][3 + 3] = true;
+					live[3 + 3][3 + 4] = true;
+					live[3 + 3][3 + 6] = true;
+					live[3 + 3][3 + 7] = true;
+					live[3 + 3][3 + 8] = true;
+					live[3 + 3][3 + 9] = true;
+					live[3 + 3][3 + 11] = true;
+					live[3 + 3][3 + 12] = true;
+					live[4 + 3][3 + 5] = true;
+					live[4 + 3][3 + 10] = true;
+				}
+				return;
+			}
+			if (m.x > 300 && m.x < 600 && m.y>400 && m.y < 600) {	//mwss
+				pass = false;
+				pulse = true;
+				for (int i = 0; i < SIZE; i++) {
+					for (int j = 0; j < SIZE; j++) {
+						live[i][j] = 0;
+					}
+				}
+				{
+					live[2][4] = true;
+					live[2][6] = true;
+					live[3][3] = true;
+					live[4][3] = true;
+					live[5][3] = true;
+					live[6][3] = true;
+					live[7][3] = true;
+					live[7][4] = true;
+					live[7][5] = true;
+					live[6][6] = true;
+					live[4][7] = true;
+				}
+				return;
+			}
+			if (m.x > 300 && m.x < 600 && m.y>600 && m.y < 800) {	//hwss
+				pass = false;
+				pulse = true;
+				for (int i = 0; i < SIZE; i++) {
+					for (int j = 0; j < SIZE; j++) {
+						live[i][j] = 0;
+					}
+				}
+				{
+					live[2][2] = true;
+					live[2][4] = true;
+					live[3][5] = true;
+					live[4][1] = true;
+					live[4][5] = true;
+					live[5][1] = true;
+					live[5][5] = true;
+					live[6][5] = true;
+					live[7][2] = true;
+					live[7][5] = true;
+					live[8][3] = true;
+					live[8][4] = true;
+					live[8][5] = true;
+				}
+				return;
+			}
+			if (m.x > 600 && m.x < 1400 && m.y>275 && m.y < 800) {	//simkin
+				pass = false;
+				pulse = true;
+				for (int i = 0; i < SIZE; i++) {
+					for (int j = 0; j < SIZE; j++) {
+						live[i][j] = 0;
+					}
+				}
+				{
+					live[5 + 1][5 + 1] = true;
+					live[5 + 2][5 + 1] = true;
+					live[5 + 1][5 + 2] = true;
+					live[5 + 2][5 + 2] = true;
+					live[5 + 8][5 + 1] = true;
+					live[5 + 8][5 + 2] = true;
+					live[5 + 9][5 + 1] = true;
+					live[5 + 9][5 + 2] = true;
+					live[5 + 5][5 + 4] = true;
+					live[5 + 5][5 + 5] = true;
+					live[5 + 6][5 + 4] = true;
+					live[5 + 6][5 + 5] = true;
+					live[5 + 22][5 + 11] = true;
+					live[5 + 22][5 + 12] = true;
+					live[5 + 22][5 + 13] = true;
+					live[5 + 22][5 + 18] = true;
+					live[5 + 21][5 + 18] = true;
+					live[5 + 21][5 + 19] = true;
+					live[5 + 22][5 + 20] = true;
+					live[5 + 23][5 + 20] = true;
+					live[5 + 24][5 + 20] = true;
+					live[5 + 24][5 + 21] = true;
+					live[5 + 23][5 + 10] = true;
+					live[5 + 23][5 + 13] = true;
+					live[5 + 24][5 + 13] = true;
+					live[5 + 24][5 + 10] = true;
+					live[5 + 26][5 + 10] = true;
+					live[5 + 27][5 + 10] = true;
+					live[5 + 28][5 + 11] = true;
+					live[5 + 29][5 + 12] = true;
+					live[5 + 28][5 + 13] = true;
+					live[5 + 27][5 + 14] = true;
+					live[5 + 32][5 + 12] = true;
+					live[5 + 32][5 + 13] = true;
+					live[5 + 33][5 + 12] = true;
+					live[5 + 33][5 + 13] = true;
 				}
 				return;
 			}
